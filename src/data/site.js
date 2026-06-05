c5e43886624fce5ab25817622e74bd37319b8e36
jessiezz219-spec <jessiezz219@gmail.com>
2026-06-05 15:04:17 +0800

    Add files via upload

---
+++ src/data/site.js
@@ -0,0 +1,20 @@
+export const site = {
+  name: "JasunBag",
+  legalName: "Shenzhen Jasun Packaging Co., Ltd.",
+  email: "info@jasunpackaging.com",
+  whatsapp: "+86 13826563762",
+  location: "Shenzhen, Guangdong, China",
+  since: "2013",
+  moq: "500 pcs/style",
+};
+
+export const nav = [
+  ["Home", "/"],
+  ["Products", "/products/"],
+  ["Customization", "/customization/"],
+  ["Sustainability", "/sustainability/"],
+  ["Gallery", "/gallery/"],
+  ["Blog", "/blog/"],
+  ["About", "/about/"],
+  ["Contact", "/contact/"],
+];
